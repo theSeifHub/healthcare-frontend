94a6f7ec7863df5a4e7221cf399e6afb6a46e642
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2023-01-20 03:32:11 +0200

    implement login cycle

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
 import { Paper, Typography, TextField, Button, Link } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 import theme from "../theme";
+import stores from "../store";
 
 const SignIn = () => {
   const { spacing } = theme;
+
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [loginError, setLoginError] = useState('');
+
+  const handleLogin = async () => {
+    setEmailError(email ? '' : 'Email is required');
+    setPasswordError(password ? '' : 'Password is required');
+    if (email && password) {
+      setLoginError('');
+      try {
+        await stores.authStore.login(email, password);
+        navigate('/');
+      } catch (err) {
+        setLoginError(`${err.status}: ${err.data.detail}`);
+      }
+    }
+  };
 
   return (
     <section style={{
@@ -36,26 +58,38 @@ const SignIn = () => {
         flexDirection: "column",
         gap: spacing(3)
       }}>
+        <div style={{ height: spacing(2) }}>
+          {!!loginError && (
+            <Typography variant="subtitle1" color="error" textAlign="center">{loginError}</Typography>
+          )}
+        </div>
         <TextField
           label="Email"
+          type="email"
           value={email}
           onChange={(evt) => setEmail(evt.currentTarget.value)}
           variant="filled"
-          fullWidth />
+          fullWidth
+          error={!!emailError}
+          helperText={emailError}
+        />
         <TextField
           label="Password"
           type={"password"}
           value={password}
           onChange={(evt) => setPassword(evt.currentTarget.value)}
           variant="filled"
-          fullWidth />
+          fullWidth
+          error={!!passwordError}
+          helperText={passwordError}
+        />
       </Paper>
 
       <Button
         variant="contained"
         color="secondary"
         size="large"
-        onClick={() => console.log('login clicked')}
+        onClick={handleLogin}
       >
         Login
       </Button>
