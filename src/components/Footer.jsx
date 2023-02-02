b7857f24df1e9f14719720bf7b00ebe9b682a5ee
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2023-02-02 14:38:10 +0200

    make footer icons interactive on hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,11 +24,63 @@ const Footer = () => {
 				height: spacing(7.5)
 			}}
 		>
-			<FacebookIcon fontSize="large" color="primary" />
-			<InstagramIcon fontSize="large" color="primary" />
-			<TwitterIcon fontSize="large" color="primary" />
-			<PinterestIcon fontSize="large" color="primary" />
-			<YouTubeIcon fontSize="large" color="primary" />
+			<FacebookIcon
+				fontSize="large"
+				color="primary"
+				sx={{
+					borderRadius: 1.5,
+					"&:hover": {
+						background: "#fff",
+						color: "#3b5998"
+					}
+				}}
+			/>
+			<InstagramIcon
+				fontSize="large"
+				color="primary"
+				sx={{
+					borderRadius: 3,
+					"&:hover": {
+						background: "linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)",
+						color: "#fff"
+					}
+				}}
+			/>
+			<TwitterIcon
+				fontSize="large"
+				color="primary"
+				sx={{
+					borderRadius: 6,
+					"&:hover": {
+						background: "#fff",
+						color: "#00acee"
+					}
+				}}
+			/>
+			<PinterestIcon
+				fontSize="large"
+				color="primary"
+				sx={{
+					borderRadius: 6,
+					"&:hover": {
+						background: "#fff",
+						color: "#c8232c"
+					}
+				}}
+			/>
+			<YouTubeIcon
+				fontSize="large"
+				color="primary"
+				sx={{
+					padding: 0,
+					margin: 0,
+					borderRadius: 2,
+					"&:hover": {
+						background: "#fff",
+						color: "#c4302b"
+					}
+				}}
+			/>
 		</footer>
 	);
 };
