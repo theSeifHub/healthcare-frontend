2d2b82dd88b08f8d6f5b8e80af7e57a6a56bfe54
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2023-05-24 22:26:37 +0300

    move appRoutes

---
+++ src/routes/index.js
@@ -0,0 +1,121 @@
+import {
+  AboutUs,
+  Booking,
+  Doctors,
+  Home,
+  SignIn,
+  SignUp,
+  DoctorSignUp,
+  PatientSignUp,
+  DoctorServicesLanding,
+  PatientServicesLanding,
+  PatientServiceBook,
+  ContactUs,
+  Clinics,
+  DoctorServices,
+  BloodBankBooking,
+  NurseryBooking,
+  CareRoomBooking,
+  SurgeryBooking,
+  DoctorProfile,
+} from '../screens';
+
+const appRoutes = [
+  {
+    path: '/',
+    element: <Home />,
+    exact: true,
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />,
+    exact: true,
+  },
+  {
+    path: '/doctor/services/land',
+    element: <DoctorServicesLanding />,
+    exact: true,
+  },
+  {
+    path: '/doctor/services',
+    element: <DoctorServices />,
+    exact: true,
+  },
+  {
+    path: '/doctor/services/bloodbank',
+    element: <BloodBankBooking />,
+    exact: true,
+  },
+  {
+    path: '/doctor/services/incubator',
+    element: <NurseryBooking />,
+    exact: true,
+  },
+  {
+    path: '/doctor/services/icu',
+    element: <CareRoomBooking />,
+    exact: true,
+  },
+  {
+    path: '/doctor/services/surgery',
+    element: <SurgeryBooking />,
+    exact: true,
+  },
+  {
+    path: '/patient/services',
+    element: <PatientServicesLanding />,
+    exact: true,
+  },
+  {
+    path: '/patient/services/book',
+    element: <PatientServiceBook />,
+    exact: true,
+  },
+  {
+    path: '/patient/services/clinics',
+    element: <Clinics />,
+    exact: true,
+  },
+  {
+    path: '/doctors',
+    element: <Doctors />,
+    exact: true,
+  },
+  {
+    path: '/booking',
+    element: <Booking />,
+    exact: true,
+  },
+  {
+    path: '/sign-up',
+    element: <SignUp />,
+    exact: true,
+  },
+  {
+    path: '/sign-up/doctor',
+    element: <DoctorSignUp />,
+    exact: true,
+  },
+  {
+    path: '/sign-up/patient',
+    element: <PatientSignUp />,
+    exact: true,
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
+    exact: true,
+  },
+  {
+    path: '/contact-us',
+    element: <ContactUs />,
+    exact: true,
+  },
+  {
+    path: '/me',
+    element: <DoctorProfile />,
+    exact: true,
+  },
+];
+
+export default appRoutes;
