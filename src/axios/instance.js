a44615e2b21c440ba19820971881e63bdea163ea
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2023-05-28 00:09:28 +0300

    enhance axios instance creation & add production baseURL

--- src/axios/instance.js
+++ src/axios/instance.js
@@ -1,11 +1,27 @@
 import axios from 'axios';
+import stores from '../stores';
 
-const baseURL = 'http://localhost:8000';
-
-const axiosInstance = axios.create({
-  baseURL,
-  timeout: 5000,
-});
+// const baseURL = 'http://localhost:8000';
+const baseURL = 'https://appointment-system-api.onrender.com';
 
 
-export default axiosInstance;
+const axiosInstance = () => {
+  const instance = axios.create({
+    baseURL,
+    timeout: 10000,
+  });
+
+  // Request interceptors 
+  instance.interceptors.request.use((reqConfig) => {
+    if (stores.authStore && stores.authStore.accessToken) {
+      reqConfig.headers.Authorization = `Bearer ${stores.authStore.accessToken}`;
+    }
+    return reqConfig;
+  });
+
+  // Response interceptors // TODO handle any error response with react-toast
+  // instance.interceptors.response.use(res => res, err => err)
+  return instance;
+};
+
+export default axiosInstance();
