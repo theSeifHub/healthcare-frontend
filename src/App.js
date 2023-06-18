646c7d046caed24ff9a3e1d33b3ca3d596b873b8
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2023-06-18 12:22:38 +0300

    fix app marginTop with navbar

--- src/App.js
+++ src/App.js
@@ -6,21 +6,26 @@ import {
 import appRoutes from "./routes";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import theme from "./theme";
 
 const App = () => {
+  const { spacing } = theme;
+
   return (
     <>
       <NavBar />
-      <Routes>
-        {appRoutes.map(r => (
-          <Route
-            key={r.path}
-            path={r.path}
-            element={r.element}
-            exact={r.exact}
-          />
-        ))}
-      </Routes>
+      <div style={{ marginTop: spacing(8) }}>
+        <Routes>
+          {appRoutes.map(r => (
+            <Route
+              key={r.path}
+              path={r.path}
+              element={r.element}
+              exact={r.exact}
+            />
+          ))}
+        </Routes>
+      </div>
       <Footer />
     </>
   );
